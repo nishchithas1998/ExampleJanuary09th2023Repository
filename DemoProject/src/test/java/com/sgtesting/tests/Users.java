cec83d46df5d7cf5671b10d6b7be8679f3370217
nischithas1998@gmail.com <nischithas1998@gmail.com>
2023-04-07 08:44:14 +0530

    Intial Draft

---
+++ DemoProject/src/test/java/com/sgtesting/tests/Users.java
@@ -0,0 +1,33 @@
+package com.sgtesting.tests;
+
+import org.testng.annotations.Test;
+
+public class Users {
+	
+	@Test
+	public void method1()
+	{
+		System.out.println("It is method1 statement");
+	}
+	
+	@Test
+	public void method2()
+	{
+		System.out.println("It is method2 statement");
+	}
+	
+	@Test
+	public void method3()
+	{
+		System.out.println("It is method3 statement");
+	}
+	
+	
+	
+	
+	
+	
+	
+
+	
+}
